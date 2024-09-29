92739d8f9894244c3a5f5a3eccac8004522b292a
Suprio Chakraborty <23201155@uap-bd.edu>
2024-09-29 20:59:32 +0600

    Create Array Reverse.c

---
+++ Array Reverse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],i;
+    printf("Enter array element: ");
+
+    for (i=0;i<=4;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=4;i>=0;i--)
+    {
+        printf("\nArray in reverse order:%d",a[i]);
+    }
+    
+    return 0;
+}
